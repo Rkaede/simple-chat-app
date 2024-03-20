e7e505fc09088020d47e7d2d1fd5a00372a1cbe2
coreym <corey.mac@gmail.com>
2024-03-20 11:21:20 +1100

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  env: {
+    APP_AWS_REGION: process.env.APP_AWS_REGION,
+    APP_AWS_ACCESS_KEY_ID: process.env.APP_AWS_ACCESS_KEY_ID,
+    APP_AWS_SECRET_ACCESS_KEY: process.env.APP_AWS_SECRET_ACCESS_KEY,
+  },
+};
 
 export default nextConfig;
